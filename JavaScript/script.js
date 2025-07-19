e65abb1f40e4562b651a5f5b6bc58104b1c24dd3
TMadhusha <madhusha200069@gmail.com>
2025-07-19 22:54:26 +0530

    commit

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,21 @@
-console.log("Page successfully loaded");
+console.log("Page successfully loaded");   
+
+var teacherName="Kalob Taulien";
+
+console.log(teacherName)
+var sum=0;
+        var arr=[10,20,30];
+        for(var i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        console.log(sum);
+
+if(teacherName == "Kalob "){
+    let apples=5;
+    const pi=3.14;
+    console.log("I have ",apples," apples");
+    console.log("The PI is ",pi);
+}else{
+    console.log("Not equal");
+}
+
