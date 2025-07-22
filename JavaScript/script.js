f7b95e4f3f8cb53de6fa9e1dfcf548662daf8a57
TMadhusha <madhusha200069@gmail.com>
2025-07-22 23:23:34 +0530

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -30,3 +30,18 @@ else{
     console.log("You cannot do anything");
 }
 
+const course="This is JavaScript Coding Course";
+console.log(course);
+
+console.log(course.search("Coding"));
+console.log(course.slice(8,18));
+console.log(course.substr(8,10));
+console.log(course.replace("Coding","Programming"));
+console.log(course) // if we replace the word "Coding" to "Programming", it will not change bacause we have used a const variable
+
+console.log(course.toUpperCase())
+console.log(course.toLowerCase());
+
+let spacy="                lost of space        ";
+console.log(spacy);
+console.log(spacy.trim());
